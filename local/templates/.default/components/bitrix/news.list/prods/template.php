1e67fd61963b1fe27430459ae8680f451d9c78a1
casino12 <istomin_84@mail.ru>
2023-08-26 01:55:34 +0400

    add another lisr with categories, but wrong

--- local/templates/.default/components/bitrix/news.list/prods/template.php
+++ local/templates/.default/components/bitrix/news.list/prods/template.php
@@ -21,16 +21,35 @@ $this->setFrameMode(true);
 <?if($arParams["DISPLAY_TOP_PAGER"]):?>
 	<?=$arResult["NAV_STRING"]?><br />
 <?endif;?>
-<?foreach($arResult["ITEMS"] as $arItem):
-	
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?><div id="<?=$this->GetEditAreaId($arItem['ID'])?>">
-        <a href="<?=$arItem['$DETAIL_PAGE_URL']?>"><?=$arItem['NAME']?></a>
-        <span><?=$arItem['PROPERTIES']['WEIGHT']['VALUE']?></span>
-        <span><?=$arItem['PROPERTIES']['COUNT']['VALUE']?></span>
-    </div>
-<?endforeach;?>
+
+<?$arFilter = array('IBLOCK_ID' => 1); 
+
+
+$rsSect = CIBlockSection::GetList(
+     Array("SORT"=>"ASC"),
+     $arSelect 
+);
+
+
+
+while ($arSect = $rsSect->GetNext()) {   
+    
+     
+    echo $arSect['NAME'];
+      
+    foreach($arResult["ITEMS"] as $arItem):?>    
+    
+        <div id="<?=$this->GetEditAreaId($arItem['ID'])?>">
+            <a href="<?=$arItem['$DETAIL_PAGE_URL']?>"><?=$arItem['NAME']?></a>
+            
+            <span><?=$arItem['PROPERTIES']['WEIGHT']['VALUE']?></span>
+            <span><?=$arItem['PROPERTIES']['COUNT']['VALUE']?></span>
+            
+        </div>
+    
+    <?endforeach;
+    
+}?>
 <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
 	<br /><?=$arResult["NAV_STRING"]?>
 <?endif;?>

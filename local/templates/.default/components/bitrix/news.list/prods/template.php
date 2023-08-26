87d2dec3d2b487ec3a960bb03e0984fe51354b81
casino12 <istomin_84@mail.ru>
2023-08-26 11:18:23 +0400

    add sections

--- local/templates/.default/components/bitrix/news.list/prods/template.php
+++ local/templates/.default/components/bitrix/news.list/prods/template.php
@@ -22,34 +22,47 @@ $this->setFrameMode(true);
 	<?=$arResult["NAV_STRING"]?><br />
 <?endif;?>
 
-<?$arFilter = array('IBLOCK_ID' => 1); 
-
-
-$rsSect = CIBlockSection::GetList(
-     Array("SORT"=>"ASC"),
-     $arSelect 
+<?$rsSections = CIBlockSection::GetList(
+    Array("SORT" => "ASC"),
+    Array(
+        "=IBLOCK_ID" => $arParams["IBLOCK_ID"],
+        "=ACTIVE"    => "Y"
+    )
 );
 
+while ($arSection = $rsSections->GetNext())
+    $arSections[] = $arSection;
+
+foreach ($arSections as $arSection){
+    foreach ($arResult["ITEMS"] as $arItem){
+        if ($arItem["IBLOCK_SECTION_ID"] == $arSection["ID"])
+            $arSection["ELEMENTS"][] = $arItem;
+    }
+    $arElementGroups[] = $arSection;
+}
+
+$arResult["ITEMS"] = $arElementGroups;
+
+foreach ($arResult["ITEMS"] as $arSection):?>
+    <h4><?=$arSection["NAME"]?></h4>
+    <?if ($arSection["ELEMENTS"]):?>
+        <ul>
+            <?foreach ($arSection["ELEMENTS"] as $arItem):?>
+                <?
+                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                ?>
+                <li id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a>
+                    <span><?=$arItem['PROPERTIES']['WEIGHT']['VALUE']?></span>
+                    <span><?=$arItem['PROPERTIES']['COUNT']['VALUE']?></span>
+                </li>
+            <?endforeach?>
+        </ul>
+    <?endif?>
+<?endforeach?>
 
 
-while ($arSect = $rsSect->GetNext()) {   
-    
-     
-    echo $arSect['NAME'];
-      
-    foreach($arResult["ITEMS"] as $arItem):?>    
-    
-        <div id="<?=$this->GetEditAreaId($arItem['ID'])?>">
-            <a href="<?=$arItem['$DETAIL_PAGE_URL']?>"><?=$arItem['NAME']?></a>
-            
-            <span><?=$arItem['PROPERTIES']['WEIGHT']['VALUE']?></span>
-            <span><?=$arItem['PROPERTIES']['COUNT']['VALUE']?></span>
-            
-        </div>
-    
-    <?endforeach;
-    
-}?>
 <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
 	<br /><?=$arResult["NAV_STRING"]?>
 <?endif;?>

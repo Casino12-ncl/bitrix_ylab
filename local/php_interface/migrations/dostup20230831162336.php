66471001f988b0544e1046b7a050a844ed077332
casino12 <istomin_84@mail.ru>
2023-08-31 17:24:09 +0400

    add migration

---
+++ local/php_interface/migrations/dostup20230831162336.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class dostup20230831162336 extends Version
+{
+    protected $description = "";
+
+    protected $moduleVersion = "4.3.2";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $helper->Iblock()->saveIblockType(array (
+  'ID' => 'private_access',
+  'SECTIONS' => 'Y',
+  'EDIT_FILE_BEFORE' => '',
+  'EDIT_FILE_AFTER' => '',
+  'IN_RSS' => 'N',
+  'SORT' => '500',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Доступ к закрытому разделу сайта',
+      'SECTION_NAME' => '',
+      'ELEMENT_NAME' => '',
+    ),
+    'en' => 
+    array (
+      'NAME' => 'Access to the private section of the site',
+      'SECTION_NAME' => '',
+      'ELEMENT_NAME' => '',
+    ),
+  ),
+));
+        $iblockId = $helper->Iblock()->saveIblock(array (
+  'IBLOCK_TYPE_ID' => 'private_access',
+  'LID' => 
+  array (
+    0 => 's1',
+  ),
+  'CODE' => 'private_access',
+  'API_CODE' => NULL,
+  'REST_ON' => 'N',
+  'NAME' => 'Доступ к закрытому разделу сайта',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'LIST_PAGE_URL' => '#SITE_DIR#/private_access/index.php?ID=#IBLOCK_ID#',
+  'DETAIL_PAGE_URL' => '#SITE_DIR#/private_access/detail.php?ID=#ELEMENT_ID#',
+  'SECTION_PAGE_URL' => '#SITE_DIR#/private_access/list.php?SECTION_ID=#SECTION_ID#',
+  'CANONICAL_PAGE_URL' => '',
+  'PICTURE' => NULL,
+  'DESCRIPTION' => '',
+  'DESCRIPTION_TYPE' => 'text',
+  'RSS_TTL' => '24',
+  'RSS_ACTIVE' => 'Y',
+  'RSS_FILE_ACTIVE' => 'N',
+  'RSS_FILE_LIMIT' => NULL,
+  'RSS_FILE_DAYS' => NULL,
+  'RSS_YANDEX_ACTIVE' => 'N',
+  'XML_ID' => NULL,
+  'INDEX_ELEMENT' => 'Y',
+  'INDEX_SECTION' => 'Y',
+  'WORKFLOW' => 'N',
+  'BIZPROC' => 'N',
+  'SECTION_CHOOSER' => 'L',
+  'LIST_MODE' => '',
+  'RIGHTS_MODE' => 'S',
+  'SECTION_PROPERTY' => 'N',
+  'PROPERTY_INDEX' => 'N',
+  'VERSION' => '1',
+  'LAST_CONV_ELEMENT' => '0',
+  'SOCNET_GROUP_ID' => NULL,
+  'EDIT_FILE_BEFORE' => '',
+  'EDIT_FILE_AFTER' => '',
+  'SECTIONS_NAME' => 'Разделы',
+  'SECTION_NAME' => 'Раздел',
+  'ELEMENTS_NAME' => 'Элементы',
+  'ELEMENT_NAME' => 'Элемент',
+  'EXTERNAL_ID' => NULL,
+  'LANG_DIR' => '/',
+  'SERVER_NAME' => NULL,
+  'IPROPERTY_TEMPLATES' => 
+  array (
+  ),
+  'ELEMENT_ADD' => 'Добавить элемент',
+  'ELEMENT_EDIT' => 'Изменить элемент',
+  'ELEMENT_DELETE' => 'Удалить элемент',
+  'SECTION_ADD' => 'Добавить раздел',
+  'SECTION_EDIT' => 'Изменить раздел',
+  'SECTION_DELETE' => 'Удалить раздел',
+));
+
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}

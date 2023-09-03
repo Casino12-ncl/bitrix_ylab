33754468bbf20da7eb3c37af53b806a555a22311
casino12 <istomin_84@mail.ru>
2023-09-03 21:52:06 +0400

    add filter

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -35,17 +35,22 @@ function deletePeople()
 
   $IBLOCK_ID = Array("IBLOCK_ID"=>2);
   $time = new DateTime(date("d.m.Y H:i:s"));
-
+  $arFilter = array(
+    "IBLOCK_ID" => $IBLOCK_ID,
+    "ACTIVE" => "Y",
+    '<DATE_ACTIVE_TO' => $time->format("d.m.Y H:i:s")
+);
   $res = CIBlockElement::GetList(
       array("ID" => "ASC"),
       array("IBLOCK_ID" => $IBLOCK_ID),
-      false,
+      $arFilter,
       false,
       [
           'IBLOCK_ID',
           'ID',
           'NAME',
-          'ACTIVE_TO'
+          'ACTIVE_TO',
+          'DATE_ACTIVE_TO'
       ]
   );
 
@@ -55,11 +60,9 @@ function deletePeople()
     $ELEMENT_ID = $arItem['ID'];           
     if (CIBlock::GetPermission($IBLOCK_ID) >= 'W') {
       $DB->StartTransaction();
-      if (!CIBlockElement::Delete($ELEMENT_ID)) {
-        if ($time->getTimestamp() > MakeTimeStamp($TIMESTAMP)) { 
+      if (!CIBlockElement::Delete($ELEMENT_ID)) {       
           $element = (int)$ELEMENT_ID;                                            
-          CIBlockElement::Delete($element);
-        }
+          CIBlockElement::Delete($element);      
         $strWarning .= 'Error!';
         $DB->Rollback();
     } else {

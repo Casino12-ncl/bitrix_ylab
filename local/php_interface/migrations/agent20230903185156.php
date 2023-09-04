f528b19ecb6eede7ef9704a026b01561f907b98d
casino12 <istomin_84@mail.ru>
2023-09-04 21:44:23 +0400

    readd agent

--- local/php_interface/migrations/agent20230903185156.php
+++ local/php_interface/migrations/agent20230903185156.php
@@ -33,6 +33,6 @@ class agent20230903185156 extends Version
     {
         $helper = $this->getHelperManager();
 
-        $status = $helper->CAgent::Delete('57');
+        $status = CAgent::Delete('57');
     }
 }
